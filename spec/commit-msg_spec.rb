7f1e6dbcd91f8f120e2f194df76ff3bcbb104d79
Bruce Woodward <bruce.woodward@gmail.com>
2014-11-17 07:58:09 +1100

    fix(status): update test to use status

--- spec/commit-msg_spec.rb
+++ spec/commit-msg_spec.rb
@@ -12,13 +12,13 @@ describe "commit-msg" do
 
   it "should write out the commit messages log" do
     input_file = make_file_with('stuff')
-    stdout, stderr = run_commit_msg
+    stdout, stderr, status = run_commit_msg
     expect(cat(input_file).first).to match(/stuff/)
   end
 
   it "should convert uppercase to lower case on the first line" do
     input_file = make_file_with('AAAAA')
-    stdout, stderr = run_commit_msg args: input_file
+    stdout, stderr, status = run_commit_msg args: input_file
     expect(cat(input_file).first).to match(/aaaaa/)
   end
 end
